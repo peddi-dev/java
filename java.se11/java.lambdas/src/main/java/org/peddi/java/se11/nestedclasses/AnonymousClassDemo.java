1012c396a13830dcf7a052a0b672adf6fddfec8b
peddi-dev <peddi.groups@gmail.com>
2019-03-11 14:38:01 +0530

    nested classes

---
+++ java.se11/java.lambdas/src/main/java/org/peddi/java/se11/nestedclasses/AnonymousClassDemo.java
@@ -0,0 +1,59 @@
+package org.peddi.java.se11.nestedclasses;
+
+public class AnonymousClassDemo {
+
+    interface HelloWorld {
+        public void greet();
+        public void greetSomeone(String someone);
+    }
+
+    public void sayHello() {
+
+        // Local class
+        class EnglishGreeting implements HelloWorld {
+            String name = "world";
+            public void greet() {
+                greetSomeone(name);
+            }
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Hello " + name);
+            }
+        }
+
+        HelloWorld englishGreeting = new EnglishGreeting();
+
+        // Anonymous class
+        HelloWorld frenchGreeting = new HelloWorld() {
+            String name = "tout le monde";
+            public void greet() {
+                greetSomeone(name);
+            }
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Salut " + name);
+            }
+        };
+
+        // Anonymous class
+        HelloWorld spanishGreeting = new HelloWorld() {
+            String name = "mundo";
+            public void greet() {
+                greetSomeone(name);
+            }
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Hola, " + name);
+            }
+        };
+        englishGreeting.greet();
+        frenchGreeting.greetSomeone("Fred");
+        spanishGreeting.greet();
+    }
+
+//    public static void main(String... args) {
+//        HelloWorldAnonymousClasses myApp =
+//                new HelloWorldAnonymousClasses();
+//        myApp.sayHello();
+//    }
+}
